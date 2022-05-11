0658f49df2009426aae574f1c960309415d9eefc
FernandoTDF <104107236+FernandoTDF@users.noreply.github.com>
2022-05-11 18:10:53 -0300

    Cambio en informe por Console.log Cambio en informe por Console.log

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ let suma:number = 0;
 
 if (num1>num2){
   for(let i=num2; i<=num1;i++){
+    let sumaPrevio:number=suma;
     suma= suma+i;
-    console.log("suma de "+i + "es " + suma);
+    console.log("suma de "+i + " + "+ sumaPrevio + " = " + suma);
   }
 } else if (num2>num1){
     for(let i=num1; i<=num2;i++){
+      let sumaPrevio:number=suma;
       suma= suma+i;
-      console.log("suma de "+i + "es " + suma);
+      console.log("suma de "+i + " + "+ sumaPrevio + " = " + suma);
     }
 } else if (num1==num2){
   console.log("ingrese dos numeros distintos");
